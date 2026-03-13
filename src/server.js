789bb1fc10ca7c64fcba821a684cc57d34168518
julia2524 <61957239+julia2524@users.noreply.github.com>
2026-03-13 10:46:45 +0900

    typing router

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.send("I still love you");
-};
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port ${PORT} 🚀`);
